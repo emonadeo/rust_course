510f7bd0d8ab0c8a4aae3b9fb8cf2eff9bc5ea08
Emanuel Pilz <emonadeo@gmail.com>
2024-05-02 16:04:06 +0200

    move `01.rs` into crate `01_hello_world`

---
+++ 01_hello_world/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let x: i32 = 5 * 6;
+    let y: i32 = 5 + 6;
+    let z: i32 = 5 - 6;
+    let s: i32 = 5 / 2;
+
+    let text: &str = "";
+
+    let f: f32 = 20.5;
+    let k: f32 = f / (x as f32);
+
+    let h: bool = true;
+    let j: bool = false;
+
+    let i: bool = h && j;
+    let l: bool = h || j;
+}
